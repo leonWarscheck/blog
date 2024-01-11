e2c7f7b523d84cae4519a13ac01b13cce0bb4e87
Leon Andersen <leon.andersen@protonmail.com>
2024-01-11 13:33:03 +0100

    indentation cleanup

--- src/components/MenuButton.js
+++ src/components/MenuButton.js
@@ -2,32 +2,31 @@ import { useState } from "react";
 import Link from "next/link";
 
 export default function Menu(){
-    const [menuOpen, setMenuOpen] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
-    const handleMenuOpen = () => {
-        setMenuOpen(!menuOpen);
-    };
+  const handleMenuOpen = () => {
+    setMenuOpen(!menuOpen);
+  };
     return(
         <div className=" block sm:hidden ml-auto">
-            <div >
-                <button onClick={handleMenuOpen}>
-                 <div
-            className="w-20 h-9 bg-no-repeat "
-            style={{ backgroundImage: `url('/BurgerDots.svg')` }}
-          ></div>
-                </button>
-            </div>
+          <div >
+            <button onClick={handleMenuOpen}>
+              <div
+                className="w-20 h-9 bg-no-repeat "
+                style={{ backgroundImage: `url('/BurgerDots.svg')` }}>
+              </div>
+            </button>
+          </div>
 
-            {menuOpen && (
-        <div className="bg-neutral-500 absolute z-40 w-full">
-      
-          <ul className="text-neutral-800 text-xl font-semibold  ">
-                   <li><Link className='text-2xl  hover:text-orange-la text-neutral-800'  href="/about">About</Link></li>
-            <li>Subscribe</li>
-          </ul>
-        </div>
-      )}
-        </div>
+          {menuOpen && (
+            <div className="bg-neutral-500 absolute z-40 w-full">
+                <ul className="text-neutral-800 text-xl font-semibold  ">
+                  <li><Link className='text-2xl  hover:text-orange-la text-neutral-800'  href="/about">About</Link></li>
+                  <li>Subscribe</li>
+                </ul>
+            </div>
+          )}
+      </div>
     )
 }
 
---
+++ src/components/alyph.js
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+
+export default function Page() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="flex h-full flex-col">
+      <header className=" fixed w-full">
+        <nav className="flex relative h-12 items-center justify-between bg-neutral-800 p-2">
+          <h1>Leon Andersen</h1>
+
+          <button className="flex gap-1" onClick={() => setIsOpen(true)}>
+            <div className="size-5 rounded-full bg-purple-500" />
+            <div className="size-5 rounded-full bg-orange-500" />
+          </button>
+
+          {isOpen && (
+            <ul className="absolute left-0 right-0 top-10 border border-border bg-gray-600 p-1">
+              <li>Subscribe</li>
+              <li>About</li>
+            </ul>
+          )}
+        </nav>
+      </header>
+
+      <main className="flex h-full flex-col">Hello!</main>
+
+      <footer>This is the footer</footer>
+    </div>
+  );
+}
