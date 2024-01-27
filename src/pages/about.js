658bf215f16fa8c91a1d34c870f8f67a1e5c63f5
Leon <leon@Jans-MacBook-Pro.local>
2024-01-27 15:06:22 +0100

    fix(ui): aboutpage text styling smaller justify breakpoints etc.

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,12 +2,12 @@ import Head from "next/head";
 
 export default function AboutPage() {
   return (
-    <main className="max-w-2xl mx-auto pt-28 pb-16 px-4 grow ">
+    <main className="max-w-2xl mx-auto pt-20 pb-12 px-4 grow ">
       <Head>
         <title>About Leon Andersen</title>
       </Head>
       <div className=" ">
-      <ul className="text-orange-la mb-5 flex pb-3 font-semibold  text-xl">
+      <ul className="text-orange-la mb-5 flex pb-1 font-medium ">
         <li>
           <h2 className="mr-2 group  underline-offset-2 hover:text-neutral-400 ">
             <a href="https://github.com/leonAndersen">Github</a>
@@ -24,11 +24,11 @@ export default function AboutPage() {
           </h2>
         </li>
       </ul>
-      <h1 className="font-bold text-4xl mb-1">Leon Andersen</h1>
+      <h1 className="font-semibold text-3xl mb-1">Leon Andersen</h1>
       <h3 className="italic text-xl mb-7 font-light">
         "Creative power is head and heart working together."
       </h3>
-      <p className="text-2xl mb-7  pb-4">
+      <p className="text-xl mb-7 text-balance c1:text-justify pb-4">
         Leon is a Fullstack Senior Developer at an awesome company, soon. <br />
         <br />
         After studying music production he explored Webdevelopment and became a
