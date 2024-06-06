37199142b65c46462ee3b8f505dce3b8249ec3da
Leon <leon@Jans-MBP.fritz.box>
2024-06-06 18:17:12 +0200

    fix(typography): set c1 breaking point for different pre xmargins

--- src/components/MdxLayout.js
+++ src/components/MdxLayout.js
@@ -4,7 +4,7 @@ export default function MdxLayout({ children }) {
   return (
     <main
       className="max-w-2xl pt-12 grow w-full mx-auto px-4 prose 
-      prose-neutral prose-invert prose-pre:bg-neutral- prose-pre:-mb-4 prose-pre:ml-4 prose-a:no-underline 
+      prose-neutral prose-invert prose-pre:bg-neutral- prose-pre:-mb-4 prose-pre:-ml-4 c1:prose-pre:ml-4 prose-a:no-underline 
       hover:prose-a:underline prose-h4:font-bold prose-blockquote:bg-neutral-500"
       role="region"
       aria-label="MDX Article Layout"
