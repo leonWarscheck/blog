0b537bcd6b1d603f9f552ff7ad23f9692d7147ff
Leon <leon@Jans-MBP.fritz.box>
2024-06-06 17:40:42 +0200

    fix(typography): replaced first brs with hrs

--- mdx-components.js
+++ mdx-components.js
@@ -2,6 +2,7 @@ export function useMDXComponents(components) {
   return {
     // pre: ({children})=> <pre>{children}</pre>,
     hr: () => <hr style={{ height: "2.7px", backgroundColor: "white" }} />,
+    br: () => <br style={{ lineHeight: "10px" }} />,
     // tasklist?
     ...components,
   };
