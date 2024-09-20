80746ffaa70904e55e34786ee37081c037d0cddc
Leon <leon.andersen@protonmail.com>
2024-09-20 02:23:46 +0200

    feat(ui): 404 color fix, no gradients allowed

--- src/pages/404.js
+++ src/pages/404.js
@@ -14,7 +14,7 @@ export default function Page404() {
   return (
     <main className="grow flex flex-col mx-auto max-w-2xl pt-20 ">
       {/* prettier-ignore */}
-      <pre className=" pt-16 text-xs c2:text-la1 px-4  xl:text-la1 w-full font-bold bg-gradient-to-b from-violet-500 to-emerald-la bg-clip-text text-transparent">
+      <pre className=" pt-16 text-xs c2:text-la1 px-4  xl:text-la1 w-full font-bold text-red-500 hover:text-yellow-la hover:bg-gradient-to -b from-red-500 to-violet-500 to-yellow- la hover:bg-clip- text hover:text- transparent">
 NOT FOUND ERROR 404/ NOT FOUND ERROR 404<br/>
 ----------------------------------------<br/>
 #          :::      :::::::      :::   #<br/>
@@ -28,7 +28,7 @@ NOT FOUND ERROR 404/ NOT FOUND ERROR 404<br/>
 NOT FOUND ERROR 404/ NOT FOUND ERROR 404<br/>
   </pre>
 
-      <h2 className="text-pink-500 text-justify px-4 font-semibold c2:text-2xl  c2:max-w-2xl max-w-xs pt-20">
+      <h2 className="hover:text-violet-500 text-emerald-la text-justify px-4 font-semibold c2:text-2xl  c2:max-w-2xl max-w-xs pt-20">
         PAGE NOT FOUND. Please check the URL in the address bar and try again.{" "}
         <Link className=" underline underline-offset-2 " href="/">
           BACK TO HOMEPAGE
